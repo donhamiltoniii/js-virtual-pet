333a2ad6f39037a9530035736f6fb532fcf1fcca
Don Hamilton III <donhamiltoniii@gmail.com>
2018-04-25 14:14:27 -0400

    Refactor

--- src/js/virtual-pet-app.js
+++ src/js/virtual-pet-app.js
@@ -1,68 +1,72 @@
-const VirtualPetApp = function() {
-	this._turn = 0
-}
-
-VirtualPetApp.prototype.run = function() {
-	let virtualPet
-	virtualPet = new VirtualPet('Wallace', 'Cat', 'Always a hungry guy')
-	while (this.userInput !== 'quit') {
-		console.log(`
-			Welcome to Donny's Virtual Pet Facility!
-			How would you like to interact with Wallace?
-
-			1. Feed
-			2. Give Drink
-			3. Clean
-			4. Check Description
-			5. Check Type of Animal
-			6. Check turn
-			Type 'quit' to exit
-		`)
-		this.userInput = prompt('What option would you like to choose?')
-		switch (this.userInput) {
-			case '1':
-				virtualPet.feed()
-				break
-			case '2':
-				virtualPet.giveDrink()
-				break
-			case '3':
-				virtualPet.clean()
-				break
-			case '4':
-				console.log(`
-					${virtualPet.getName()} is ${virtualPet.getDescription()}
-				`)
-				break
-			case '5':
-				console.log(`
-					Animal type is: ${this.getType()}
-				`)
-				break
-			case '6':
-				console.log(`
-					Turn number: ${this.getTurn()}
-				`)
-				break
-			default:
-				break
-		}
-		console.log(`
-			${virtualPet.getName()} the ${virtualPet.getType()}'s Stats:
-
-			Hunger: ${virtualPet.getHunger()}
-			Thirst: ${virtualPet.getThirst()}
-			Cleanliness: ${virtualPet.getCleanliness()}
-		`)
-		virtualPet.tick()
-		this._turn++
+const VirtualPetApp = (function() {
+	function VirtualPetApp() {
+		this._turn = 0
 	}
-	return true
-}
 
-VirtualPetApp.prototype.getTurn = function() {
-	return this._turn
-}
-VirtualPetApp.prototype.getUserInput = function() {
-	return this.userInput
-}
+	VirtualPetApp.prototype.run = function() {
+		let virtualPet
+		virtualPet = new VirtualPet('Wallace', 'Cat', 'Always a hungry guy')
+		while (this.userInput !== 'quit') {
+			console.log(`
+				Welcome to Donny's Virtual Pet Facility!
+				How would you like to interact with Wallace?
+
+				1. Feed
+				2. Give Drink
+				3. Clean
+				4. Check Description
+				5. Check Type of Animal
+				6. Check turn
+				Type 'quit' to exit
+			`)
+			this.userInput = prompt('What option would you like to choose?')
+			switch (this.userInput) {
+				case '1':
+					virtualPet.feed()
+					break
+				case '2':
+					virtualPet.giveDrink()
+					break
+				case '3':
+					virtualPet.clean()
+					break
+				case '4':
+					console.log(`
+						${virtualPet.getName()} is ${virtualPet.getDescription()}
+					`)
+					break
+				case '5':
+					console.log(`
+						Animal type is: ${virtualPet.getType()}
+					`)
+					break
+				case '6':
+					console.log(`
+						Turn number: ${this.getTurn()}
+					`)
+					break
+				default:
+					break
+			}
+			console.log(`
+				${virtualPet.getName()} the ${virtualPet.getType()}'s Stats:
+
+				Hunger: ${virtualPet.getHunger()}
+				Thirst: ${virtualPet.getThirst()}
+				Cleanliness: ${virtualPet.getCleanliness()}
+			`)
+			virtualPet.tick()
+			this._turn++
+		}
+		return true
+	}
+
+	VirtualPetApp.prototype.getTurn = function() {
+		return this._turn
+	}
+	VirtualPetApp.prototype.getUserInput = function() {
+		return this.userInput
+	}
+
+	return VirtualPetApp
+})()
--- src/js/virtual-pet.js
+++ src/js/virtual-pet.js
@@ -1,43 +1,47 @@
-const VirtualPet = function(name, type, description) {
-	this.name = name
-	this.type = type
-	this.description = description
+const VirtualPet = (function() {
+	function VirtualPet(name, type, description) {
+		this.name = name
+		this.type = type
+		this.description = description
 
-	this._hunger = 10
-	this._thirst = 10
-	this._cleanliness = 10
-}
+		this._hunger = 10
+		this._thirst = 10
+		this._cleanliness = 10
+	}
 
-VirtualPet.prototype.getCleanliness = function() {
-	return this._cleanliness
-}
-VirtualPet.prototype.getDescription = function() {
-	return this.description
-}
-VirtualPet.prototype.getHunger = function() {
-	return this._hunger
-}
-VirtualPet.prototype.getName = function() {
-	return this.name
-}
-VirtualPet.prototype.getThirst = function() {
-	return this._thirst
-}
-VirtualPet.prototype.getType = function() {
-	return this.type
-}
+	VirtualPet.prototype.getCleanliness = function() {
+		return this._cleanliness
+	}
+	VirtualPet.prototype.getDescription = function() {
+		return this.description
+	}
+	VirtualPet.prototype.getHunger = function() {
+		return this._hunger
+	}
+	VirtualPet.prototype.getName = function() {
+		return this.name
+	}
+	VirtualPet.prototype.getThirst = function() {
+		return this._thirst
+	}
+	VirtualPet.prototype.getType = function() {
+		return this.type
+	}
 
-VirtualPet.prototype.clean = function() {
-	this._cleanliness += 3
-}
-VirtualPet.prototype.feed = function() {
-	this._hunger -= 3
-}
-VirtualPet.prototype.giveDrink = function() {
-	this._thirst -= 3
-}
-VirtualPet.prototype.tick = function() {
-	this._cleanliness -= 1
-	this._hunger += 1
-	this._thirst += 1
-}
+	VirtualPet.prototype.clean = function() {
+		this._cleanliness += 3
+	}
+	VirtualPet.prototype.feed = function() {
+		this._hunger -= 3
+	}
+	VirtualPet.prototype.giveDrink = function() {
+		this._thirst -= 3
+	}
+	VirtualPet.prototype.tick = function() {
+		this._cleanliness -= 1
+		this._hunger += 1
+		this._thirst += 1
+	}
+
+	return VirtualPet
+})()
