ce112c9c32d0214ac899137152070063716053d3
Don Hamilton III <donhamiltoniii@gmail.com>
2018-04-25 10:53:17 -0400

    Add tick method

--- spec/js/virtual-pet.spec.js
+++ spec/js/virtual-pet.spec.js
@@ -1,39 +1,82 @@
 describe('Virtual Pet', function() {
 	it('should have instance variables', function() {
-		const virtualPet = new VirtualPet()
+		const virtualPet = new VirtualPet('Pet', 'Animal', 'Generic animal')
 		expect(virtualPet.getHunger()).toEqual(10)
 		expect(virtualPet.getThirst()).toEqual(10)
 		expect(virtualPet.getCleanliness()).toEqual(10)
 	})
 
-	describe('feed()', function() {
-		it('should reduce hunger', function() {
-			const wallace = new VirtualPet()
-			const expected = wallace.getHunger() - 3
-			wallace.feed()
-			const underTest = wallace.getHunger()
+	describe('instance modifiers', function() {
+		describe('feed()', function() {
+			it('should reduce hunger', function() {
+				const wallace = new VirtualPet(
+					'Wallace',
+					'Cat',
+					'Super hungry always',
+				)
+				const expected = wallace.getHunger() - 3
+				wallace.feed()
+				const underTest = wallace.getHunger()
 
-			expect(expected).toEqual(underTest)
+				expect(expected).toEqual(underTest)
+			})
+		})
+
+		describe('giveDrink()', function() {
+			it('should reduce thirst', function() {
+				const maurice = new VirtualPet(
+					'Momo',
+					'Kitten',
+					'Wants to cuddle',
+				)
+				const expected = maurice.getThirst() - 3
+				maurice.giveDrink()
+				const underTest = maurice.getThirst()
+
+				expect(expected).toEqual(underTest)
+			})
+		})
+
+		describe('clean()', function() {
+			it('should increase cleanliness', function() {
+				const jojo = new VirtualPet(
+					'Jojo',
+					'Cat',
+					'Runs around all night and keeps up owners',
+				)
+				const expected = jojo.getCleanliness() + 3
+				jojo.clean()
+				const underTest = jojo.getCleanliness()
+
+				expect(expected).toEqual(underTest)
+			})
 		})
 	})
 
-	describe('giveDrink()', function() {
-		it('should reduce thirst', function() {
-			const maurice = new VirtualPet()
-			const expected = maurice.getThirst() - 3
-			maurice.giveDrink()
-			const underTest = maurice.getThirst()
+	describe('tick()', function() {
+		const billy = new VirtualPet(
+			'Bill Bill',
+			'Cat',
+			'Sleeps on my favorite blanket',
+		)
+		it('should increase hunger', function() {
+			const expected = billy.getHunger() + 1
+			billy.tick()
+			const underTest = billy.getHunger()
 
 			expect(expected).toEqual(underTest)
 		})
-	})
+		it('should increase thirst', function() {
+			const expected = billy.getThirst() + 1
+			billy.tick()
+			const underTest = billy.getThirst()
 
-	describe('clean()', function() {
-		it('should increase cleanliness', function() {
-			const jojo = new VirtualPet()
-			const expected = jojo.getCleanliness() + 3
-			jojo.clean()
-			const underTest = jojo.getCleanliness()
+			expect(expected).toEqual(underTest)
+		})
+		it('should decrease cleanliness', function() {
+			const expected = billy.getCleanliness() - 1
+			billy.tick()
+			const underTest = billy.getCleanliness()
 
 			expect(expected).toEqual(underTest)
 		})
--- src/js/virtual-pet.js
+++ src/js/virtual-pet.js
@@ -1,7 +1,15 @@
-const VirtualPet = function() {
+const VirtualPet = function(name, type, description) {
+	this.name = name
+	this.type = type
+	this.description = description
+
 	this._hunger = 10
 	this._thirst = 10
 	this._cleanliness = 10
+}
+
+VirtualPet.prototype.getCleanliness = function() {
+	return this._cleanliness
 }
 VirtualPet.prototype.getHunger = function() {
 	return this._hunger
@@ -9,16 +17,18 @@ VirtualPet.prototype.getHunger = function() {
 VirtualPet.prototype.getThirst = function() {
 	return this._thirst
 }
-VirtualPet.prototype.getCleanliness = function() {
-	return this._cleanliness
-}
 
+VirtualPet.prototype.clean = function() {
+	this._cleanliness += 3
+}
 VirtualPet.prototype.feed = function() {
 	this._hunger -= 3
 }
 VirtualPet.prototype.giveDrink = function() {
 	this._thirst -= 3
 }
-VirtualPet.prototype.clean = function() {
-	this._cleanliness += 3
+VirtualPet.prototype.tick = function() {
+	this._cleanliness -= 1
+	this._hunger += 1
+	this._thirst += 1
 }
